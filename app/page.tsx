0f86aef9ddf75e6ea80fb0f001b0dbcc84559f24
noman9k <chnoman503@gmail.com>
2025-01-03 02:19:07 +0500

    remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import CenteredCTAText from "./components/ui/CenteredCTAText";
 import CTA from "./components/ui/CTA";
 import FAQs from "./components/ui/FAQs";
@@ -7,8 +6,6 @@ import FooterCTA from "./components/ui/FooterCTA";
 import Hero from "./components/ui/Hero";
 import LogoGrid from "./components/ui/LogoGrid";
 import Stats from "./components/ui/Stats";
-import Navbar from "./components/ui/Navbar";
-import Footer from "./components/ui/Footer";
 
 export default function Home() {
   return ( <>
